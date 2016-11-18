a6e60f04a54a70a7179ccc4da0698d666b3ad351
Sevak Mardirosian <smardirosian@cosine.nl>
2016-11-18 11:19:35 +0100

    small changes

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'server',
     'server.models',
     'rest_framework'
 ]
@@ -51,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
--- server/models/models.py
+++ server/models/models.py
@@ -1,37 +1,37 @@
 from django.db import models
 from django.utils import timezone
-from server.models.core import TimeStampedModel
+#from server.models.core import TimeStampedModel
 
 
-class Users(TimeStampedModel):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
+class Users(models.Model):
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    username = models.CharField(max_length=50, null=False)
 
 
-class AccessExpire(TimeStampedModel):
+class AccessExpire(models.Model):
     user = models.ForeignKey(Users)
-    expire_date = models.DateTimeField(default=timezone.now)
+    expire_date = models.DateTimeField(default=timezone.now, null=False)
 
 
-class NetworkClass(TimeStampedModel):
-    name = models.CharField(max_length=50)
+class NetworkClass(models.Model):
+    name = models.CharField(max_length=25, null=False, unique=True)
+    description = models.CharField(max_length=255, null=True)
 
 
-class MacAddress(TimeStampedModel):
+class Computer(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=255, null=True)
+
+
+class MacAddress(models.Model):
     mac_address = models.CharField(max_length=50, null=False)
     date = models.DateField(auto_now=True)
     network = models.OneToOneField(NetworkClass)
     computer = models.ForeignKey(Computer)
     expire_date = models.OneToOneField(AccessExpire)
+    user = models.ManyToManyField(Users)
 
-
-class Computer(TimeStampedModel):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-    mac_address = models.ForeignKey(MacAddress)
-
-
-class ComputerCustodian(TimeStampedModel):
+class ComputerCustodian(models.Model):
     computer = models.ForeignKey(Computer)
     user = models.OneToOneField(Users)
